0217f8fda3995ad20deeeaf867c6c5ac37bb5067
stephanpoelwijk <stephan.poelwijk@gmail.com>
2022-10-14 01:18:03 +0200

    Remove well-known dev connectionstring

--- run-localazurite.ps1
+++ run-localazurite.ps1
@@ -22,6 +22,5 @@ $localDataFolderName = getWslPath $fullLocalDataFolder
 
 "`nLocal development connection string:"
 "UseDevelopmentStorage=true"
-"DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;"
 
 docker run -p 10000:10000 -p 10001:10001 -p 10002:10002 --mount "type=bind,source=$localDataFolderName,target=/data" mcr.microsoft.com/azure-storage/azurite

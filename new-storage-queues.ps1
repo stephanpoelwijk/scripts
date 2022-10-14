5c5fe6839d6c7d4543d50fc1e6bfd49ec6ad00a8
stephanpoelwijk <stephan.poelwijk@gmail.com>
2022-10-14 16:39:50 +0200

    Change connectionstring so it works with latest az

--- new-storage-queues.ps1
+++ new-storage-queues.ps1
@@ -2,7 +2,7 @@ param (
     [Parameter(Mandatory = $True)]
     [string] $queueNames,
     [Parameter()]
-    [string] $connectionString = "UseDevelopmentStorage=true"
+    [string] $connectionString = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;"
 )
 
 $queueNamesArray = [array] $queueNames.split(",").trim()

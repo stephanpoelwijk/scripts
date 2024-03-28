0d4d8794523e06039696e046064e1c1d6bc96507
stephanpoelwijk <stephan.poelwijk@gmail.com>
2024-03-29 00:16:00 +0100

    Add option to pull the image to the run* scripts

--- run-azurite.ps1
+++ run-azurite.ps1
@@ -1,7 +1,14 @@
 param (
     [Parameter()]
-    [string] $localDataFolder = ".azurite"
+    [string] $localDataFolder = ".azurite",
+
+    [Parameter(Mandatory = $False)]
+    [switch] $pullImage = $False
 )
+
+if ($pullImage) { 
+    docker pull mcr.microsoft.com/azure-storage/azurite
+}
 
 function getWslPath ($folderItem) {
 
@@ -24,4 +31,4 @@ $localDataFolderName = getWslPath $fullLocalDataFolder
 "UseDevelopmentStorage=true"
 "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;"
 
-docker run -p 10000:10000 -p 10001:10001 -p 10002:10002 --mount "type=bind,source=$localDataFolderName,target=/data" mcr.microsoft.com/azure-storage/azurite
+docker run -p 127.0.0.1:10000:10000 -p 127.0.0.1:10001:10001 -p 127.0.0.1:10002:10002  --mount "type=bind,source=$localDataFolderName,target=/data" mcr.microsoft.com/azure-storage/azurite

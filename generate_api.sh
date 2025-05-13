6acbacd62a417bb5119779ab448de3bfa14ca1ea
Stephan Poelwijk <stephan@poelwijkconsultancy.com>
2025-05-13 11:45:58 +0200

    Add documentation url

--- generate_api.sh
+++ generate_api.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+
+
+# Documentation can be found on an older package version:
+# https://www.npmjs.com/package/swagger-typescript-api/v/13.0.22
+
 npx swagger-typescript-api generate --path $1 --yes
 
